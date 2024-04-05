d1fba7fb418d88071b4557eea2ad45d05f88d4f0
Igor Borges <borgesigor153@gmail.com>
2024-04-04 22:03:12 -0300

    :recycle: refactor: refatorando o controller

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -2,43 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\AlunoRequest;
 use App\Models\Aluno;
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\AlunoRequest;
+use Illuminate\Database\Eloquent\Collection;
 
 class AlunoController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Collection
      */
-    public function index()
+    public function index(): Collection
     {
-        return response(Aluno::get(), 200, []);
+        return Aluno::get();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  AlunoRequest $request
+     * @return Response
      */
-    public function store(AlunoRequest $request)
+    public function store(AlunoRequest $request): Response
     {
-
-        $dadosAluno = $request->all();
-
-        return response(Aluno::create($dadosAluno), 201);
+        return response(Aluno::create($request->all()), 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  Aluno $aluno
+     * @return Aluno
      */
-    public function show(Aluno $aluno)
+    public function show(Aluno $aluno): Aluno
     {
         return $aluno;
     }
@@ -46,19 +44,12 @@ class AlunoController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  AlunoRequest $request
+     * @param  Aluno $aluno
+     * @return Aluno
      */
-    public function update(Request $request, Aluno $aluno)
+    public function update(AlunoRequest $request, Aluno $aluno): Aluno
     {
-        $request->validate([
-            'nome' => ['required', 'string', 'between:2,100'],
-            'nascimento' => ['required', 'date'],
-            'genero' => ['required', 'size:1'],
-            'turma_id' => ['required', 'int', 'exists:turmas,id']
-        ]);
-
         $aluno->update($request->all());
 
         return $aluno;
@@ -67,8 +58,8 @@ class AlunoController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  Aluno $aluno
+     * @return array
      */
     public function destroy(Aluno $aluno)
     {
--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/alunos', [AlunoController::class, 'index'])->name('alunos.index');
-
-Route::get('alunos/{aluno}', [AlunoController::class, 'show'])->name('alunos.show');
-
-Route::post('/alunos', [AlunoController::class, 'store'])->name('alunos.store');
-
-Route::put('/alunos/{aluno}', [AlunoController::class, 'update'])->name('alunos.update');
-
-Route::delete('alunos/{aluno}', [AlunoController::class, 'destroy'])->name('alunos.destroy');
-
+Route::apiResource('alunos', AlunoController::class);
