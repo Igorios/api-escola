54208015731335617bbedc33888d0765ecee2bb3
Igor Borges <borgesigor153@gmail.com>
2024-04-07 15:58:13 -0300

    :recycle: refactor: tratamento de erro

--- app/Exceptions/ApiHandler.php
+++ app/Exceptions/ApiHandler.php
@@ -3,33 +3,62 @@
 namespace App\Exceptions;
 
 use Throwable;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
+trait ApiHandler
+{
 
-trait ApiHandler {
-
-    public function tratarErros(Throwable $exception) {
+    public function tratarErros(Throwable $exception)
+    {
 
         if ($exception instanceof ModelNotFoundException) {
-            return response([
-                "code" => "registro-nao-encontrado",
-                "message" => "Não encontramos o registro que você procura",
-                "status" => 404
-            ], 404);
+            return $this->modelNotFoundException();
         }
 
-        if ($exception instanceof ValidationException) {
 
-            return response([
-                "code" => "erro-na-validacao-dos-dados",
-                "message" => "Não foi possível validar os dados",
-                "status" => 400,
-                "erros" => $exception->errors()
-            ], 400);
+        if ($exception instanceof ValidationException) {
+            return $this->validationException($exception);
         }
 
     }
 
+    public function modelNotFoundException()
+    {
+        return $this->respostaPadrao(
+            "registro-nao-encontrado",
+            "Não encontramos o registro que você procura",
+            404
+        );
+    }
 
+    public function validationException(validationException $e)
+    {
+
+        return $this->respostaPadrao(
+            "erro-na-validacao-dos-dados",
+            "Não foi possível validar os dados",
+            400,
+            $e->errors()
+        );
+    }
+
+    public function respostaPadrao(string $code, string $mensagem, int $status, array $erros = null)
+    {
+
+        $dadosResposta = [
+            "code" => $code,
+            "message" => $mensagem,
+            "status" => $status,
+        ];
+
+        if ($erros) {
+            $dadosResposta = $dadosResposta + ['erros' => $erros];
+        }
+
+        return response(
+            $dadosResposta,
+            $status
+        );
+    }
 }
